68eb0c956161298a0ebed5e83e3ba3a971d5b3cf
DESKTOP-K74ILC1\Uzytkownik <sq9ias@wp.pl>
2018-10-03 12:10:07 +0200

    Dodano logowanie

--- src/main/java/com/company/przychodnia/service/impl/WiadomosciServiceImpl.java
+++ src/main/java/com/company/przychodnia/service/impl/WiadomosciServiceImpl.java
@@ -3,6 +3,8 @@ package com.company.przychodnia.service.impl;
 import com.company.przychodnia.domain.Wiadomosc;
 import com.company.przychodnia.repository.WiadomosciRepository;
 import com.company.przychodnia.service.WiadomosciService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,13 +15,16 @@ public class WiadomosciServiceImpl implements WiadomosciService {
 
     private final WiadomosciRepository wiadomosciRepository;
 
-   @Autowired
+    private final static Logger logger = LoggerFactory.getLogger(WiadomosciServiceImpl.class);
+
+    @Autowired
     public WiadomosciServiceImpl(WiadomosciRepository wiadomosciRepository) {
         this.wiadomosciRepository = wiadomosciRepository;
     }
 
     @Override
     public List<Wiadomosc> pobierzWszytkieWiadomosci() {
+        logger.debug("Wywołano com.company.przychodnia.service.impl.pobierzWszytkieWiadomosci()");
         return wiadomosciRepository.findAll();
     }
 
@@ -27,6 +32,7 @@ public class WiadomosciServiceImpl implements WiadomosciService {
     public List<Wiadomosc> pobierzOstatnieWiadomosci(int liczba) {
         List<Wiadomosc> wiadomosci = wiadomosciRepository.findAll();
         //return wiadomosci.subList(wiadomosci.size()-liczba, wiadomosci.size());
+        logger.debug("Wywołano com.company.przychodnia.service.impl.pobierzOstatnieWiadomosci({})", liczba);
         return wiadomosciRepository.findAll();
    }
 }
