5f06e03216c81aaf775f3f0c0a02966e6b6a8b2c
kit <kit@kit.com>
2019-12-29 10:00:38 +0100

    Wyrównano przyciski na stronie głównej.

--- src/main/java/com/company/przychodnia/domain/Wiadomosc.java
+++ src/main/java/com/company/przychodnia/domain/Wiadomosc.java
@@ -4,10 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-
 import java.time.LocalDateTime;
 import java.util.Objects;
 
